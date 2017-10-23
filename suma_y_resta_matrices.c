efe4a6cac4b76a6686d1c7c70a92797a18936bde
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-10-23 13:49:24 -0500

    Add files via upload

--- suma_y_resta_matrices.c
+++ suma_y_resta_matrices.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define fil 2
 #define col 2
 
@@ -8,12 +10,19 @@ int main(){
 	int matriz[col][fil];
 	int matriz1[col][fil];
 	int matriz2[col][fil];
-	char respuesta;
+	char respuesta1;
+	char respuesta2;
+
+	printf("Este programa realiza la suma o resta de matrices a continuacion \ningresa los datos de cada matriz o ingresa R para llenarlas con numeros aleatorios. \n\n");
+	scanf("%c",&respuesta1);
 	
-	printf("Para realizar una suma digite una s o si es resta cualquier letra. \n");
-	scanf("%c",&respuesta);
-	
-	if (respuesta == 's' || respuesta == 'S'){
+	if (respuesta1 == 'r' || respuesta1 == 'R'){
+		int min, max;
+		int random1 = min + rand() / (RAND_MAX / (max - min + 1) + 1);
+		printf("%d", random1);
+		
+	}
+	else{
 		printf("Ingrese los valores para la primer matriz: \n");
 		for(i = 0; i < col; i++){
 			for(j = 0; j < fil; j++){
@@ -29,24 +38,30 @@ int main(){
 				scanf("%d",&matriz1[i][j]);
 			}
 		}
-		printf("\n");
-		printf("La primer matriz es: \n");
-		printf("\n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("\t %d ", matriz[i][j]);
-			}
-			printf("\n");
+	}
+	printf("\n");
+	printf("La primer matriz es: \n");
+	printf("\n");
+	for(i = 0; i < col; i++){
+		for(j = 0; j < fil; j++){
+			printf("\t %d ", matriz[i][j]);
 		}
 		printf("\n");
-		printf("La segunda matriz es: \n");
-		printf("\n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("\t %d ", matriz1[i][j]);
-			}
-			printf("\n");
+	}
+	printf("\n");
+	printf("La segunda matriz es: \n");
+	printf("\n");
+	for(i = 0; i < col; i++){
+		for(j = 0; j < fil; j++){
+			printf("\t %d ", matriz1[i][j]);
 		}
+		printf("\n");
+	}
+	
+	printf("Para realizar una suma digite una S o si es resta cualquier letra. \n");
+	scanf("%c",&respuesta2);
+	
+	if (respuesta2 == 's' || respuesta2 == 'S'){
 		printf("\n El resultado de la suma de ambas matrices es:\n\n");
 		for(i = 0; i < col ; i++){
 			for(j = 0; j < fil; j++){
@@ -57,41 +72,6 @@ int main(){
 		}
 	}
 	else{
-		
-		printf("Ingrese los valores para la primer matriz: \n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("Valor para [%d][%d] ", i, j);
-				scanf("%d",&matriz[i][j]);
-			}
-		}
-		printf("\n");
-		printf("Ingrese los valores para la segunda matriz: \n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("Valor para [%d][%d] ", i, j);
-				scanf("%d",&matriz1[i][j]);
-			}
-		}
-		printf("\n");
-		printf("La primer matriz es: \n");
-		printf("\n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("\t %d ", matriz[i][j]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-		printf("La segunda matriz es: \n");
-		printf("\n");
-		for(i = 0; i < col; i++){
-			for(j = 0; j < fil; j++){
-				printf("\t %d ", matriz1[i][j]);
-			}
-			printf("\n");
-		}
-		
 		printf("\n El resultado de la resta de ambas matrices es:\n\n");
 		for(i = 0; i < col ; i++){
 			for(j = 0; j < fil; j++){
@@ -100,7 +80,6 @@ int main(){
 			}
 			printf("\t\t\n\n");
 		}
-		
 	}
 	
 	return 0;
